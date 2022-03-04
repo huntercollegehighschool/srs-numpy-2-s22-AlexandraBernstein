1ae72e6c363b7bea958aa419bcb4249ea4087729
AlexandraBern <79848052+AlexandraBernstein@users.noreply.github.com>
2022-03-04 17:36:22 +0000

    wrote code

--- AColumnCypher.py
+++ AColumnCypher.py
@@ -18,28 +18,35 @@ In this exercise we will use numpy multidimensional arrays to encode a message a
 """
 1. Import numpy (as np) so that you have access to all numpy methods. Code: import numpy as np
 """
-
+import numpy as np
 
 """
 2. A message is defined below. Use np.array(<list>) to convert message into an array of characters, then print the array (note: you will have to convert message to a LIST of characters first.)
 
 """
 message = "THISMESSAGEISVERYSECRETX"
-
+message = list(message)
+a = np.array(message)
+print(a)
 
 
 """
 3. Use <array>.shape = (<number of rows>, <number of columns>) to create a column cypher with 3 columns. Print the result
 Note: You can calculate how many rows you will need, or the shape function can do that automatically if you make the number of rows -1.
 """
-
-
+a.shape = (-1,3)
+print(a)
 """
 4. Use <var> = np.transpose(<array>) to switch the rows and the columns. Print the new array.
 """
-
+b = np.transpose(a)
+print(b)
 """
 5. When you index the transposed array, you should get a row of the array. For example, <array>[0] should be the first row of characters.
 
 Declare a new variable that will store the encoded message, and set it to an empty string. Loop through the rows of the array. Use ''.join(<array>) to convert the array of characters to a string. Then add that string to the encoded message. Print the final result.
 """
+c = ""
+for row in b:
+  c = c + ''.join(row)
+print(c)
--- BPercentDailyReturn.py
+++ BPercentDailyReturn.py
@@ -9,3 +9,11 @@ Below you are given the stock prices (there are only 4 days in this example). Fi
 """
 
 acme = [10, 11.5, 11, 10, 12]
+import numpy as np
+a = np.array(acme)
+day1 = a[0:2]
+day2 = a[1:3]
+day3 = a[2:4]
+day4 = a[3:5]
+
+print(100*(day1[1]-day1[0])/day1[0])
